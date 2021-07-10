f0d52680a1287ada9beefed849fe853508b5f026
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-07-10 03:49:26 -0300

    Chicken layup

--- src/menu/menu.gd
+++ src/menu/menu.gd
@@ -4,6 +4,6 @@ onready var anim = $anims
 var opening_out : bool = false
 
 func _input(event):
-	if Input.is_action_just_pressed("accept") and !opening_out:
+	if Input.is_action_just_pressed("accept") and !opening_out and !anim.is_playing():
 		opening_out = true
 		anim.play("fade-opening")
