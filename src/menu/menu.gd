2e0ac446d91bdfa8903d490c4fa60bdee8fd7170
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-07-09 21:52:31 -0300

    Process? Who needs that?

--- src/menu/menu.gd
+++ src/menu/menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var anim = $anims
 var opening_out : bool = false
 
-func _process(delta):
-	if Input.is_action_just_pressed("accept") and !opening_out:
+func _input(event):
+	if event.is_action_just_pressed("accept") and !opening_out:
 		opening_out = true
 		anim.play("fade-opening")
