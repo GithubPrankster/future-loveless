ab82a22f93f8489bb4f2995966604277c2c2e343
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-07-10 20:13:39 -0300

    Race conditions are so cool

--- src/common/Info.gd
+++ src/common/Info.gd
@@ -1,3 +1,8 @@
 extends Node
 
+# Get it out of the way PLS
 var opening_out : bool = false
+
+# Who are we playing as?
+enum {PLAYER_TREBLE = 0, PLAYER_CASEY = 1}
+var chosen_player : int = PLAYER_TREBLE
