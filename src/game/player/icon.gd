49c56cafd157b9483583f20e5a09b0c5e3f8562f
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-07-12 19:41:19 -0300

    Add bar to icons lol

--- src/game/player/icon.gd
+++ src/game/player/icon.gd
@@ -1,10 +1,11 @@
 extends Sprite
 
 onready var anim = $anim
+onready var health = $health
 
 func ouchy(hp : int) -> void:
 	anim.play("hurt")
-	# TODO: Health bar shenanigans
+	health.value = hp
 
 func _ready():
 	Info.connect("player_hurt", self, "ouchy")
