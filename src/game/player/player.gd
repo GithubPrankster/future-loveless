c9e37b60d0c3547c3ce604624e9fc0e7f618f41f
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-07-12 20:38:04 -0300

    Attack, defend states

--- src/game/player/player.gd
+++ src/game/player/player.gd
@@ -2,17 +2,20 @@ extends KinematicBody2D
 class_name Player
 
 # State Machine
-enum PlayerState {MOVIN,ATTACKIN}
+enum PlayerState {MOVIN,ATTACKIN,CASTIN,DEFEND,HURT,DEAD}
 var state = PlayerState.MOVIN
+
 # Logic
 export(float) var FORCE  = 160.0
 var ACC : float = FORCE * 4.0
 
-var ATK_FORCE : float = FORCE * 32.0
-var ATK_ACC : float = 16.0
+var ATK_FORCE : float = FORCE * 8.0
+var ATK_ACC : float = 4.0
 
 var velocity : Vector2 = Vector2.ZERO
-var last_dir : Vector2 = Vector2.ZERO
+var last_dir : Vector2 = Vector2.LEFT
+
+onready var atk_timer = $atk_timer
 
 # Animation
 onready var chr = $chr
@@ -27,23 +30,31 @@ func move(dt : float) -> void:
 	var cur_acc : float = ACC * dt
 	
 	if vel != Vector2.ZERO:
+		# WALKIN
 		velocity = velocity.move_toward(vel * FORCE, cur_acc)
 		last_dir = vel
 	else:
+		# IDLIN (for lamers)
 		velocity = velocity.move_toward(Vector2.ZERO, cur_acc)
 	
 	if Input.is_action_just_pressed("attack"):
-		velocity = velocity.move_toward(last_dir * ATK_FORCE, cur_acc * ATK_ACC)
+		state = PlayerState.ATTACKIN
+		atk_timer.start()
 	
-	velocity = move_and_slide(velocity)
+	if Input.is_action_just_pressed("defend"):
+		state = PlayerState.DEFEND
+		atk_timer.start()
 	
 	if vel.x > 0.0:
-		chr.scale.x = 1.0
-	elif vel.x < 0.0:
 		chr.scale.x = -1.0
+	elif vel.x < 0.0:
+		chr.scale.x = 1.0
 
 func attack() -> void:
-	pass
+	velocity = last_dir * FORCE
+
+func defend() -> void:
+	velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	match(state):
@@ -51,7 +62,14 @@ func _physics_process(delta):
 			move(delta)
 		PlayerState.ATTACKIN:
 			attack()
+		PlayerState.DEFEND:
+			defend()
+	velocity = move_and_slide(velocity)
 
 func _process(_delta) -> void:
 	scale.x = (global_position.y / get_viewport().size.y) * 1.75
 	scale.y = scale.x
+
+# PLACEHOLDER UNTIL ANIM COMES ALONG!
+func atk_timeout():
+	state = PlayerState.MOVIN
