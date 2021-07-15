f3c289608d66b4eee40d2c0e869bcd11bb4cc9d0
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-07-15 03:13:27 -0300

    Iceeey girl

--- src/game/magic/ice.gd
+++ src/game/magic/ice.gd
@@ -2,11 +2,19 @@ extends KinematicBody2D
 
 var velocity : Vector2 = Vector2.ZERO
 
+onready var anim = $anim
+
 func _physics_process(_delta):
 	velocity = move_and_slide(velocity)
 
 func body_entered(_body):
+	velocity = Vector2.ZERO
+	anim.play("death")
+	yield(anim,"animation_finished")
 	queue_free()
 
 func area_entered(_area):
+	velocity = Vector2.ZERO
+	anim.play("death")
+	yield(anim,"animation_finished")
 	queue_free()
